93a6b9e85280d9046482ba5856c63bba94f8e22e
tony stark <mehul.chopra.dev@gmail.com>
2020-02-11 15:30:30 +0530

    day 4

---
+++ com/abc/college/student.py
@@ -0,0 +1,32 @@
+# Roy
+class Student:
+
+    # Called at the time of object creation
+    # Has code that creates attributes in an object
+    # Constructor
+    def __init__(self, name, roll, gender, marks):
+        self.name = name
+        self.roll = roll
+        self.gender = gender
+        self.marks = marks
+
+    def get_details(self):
+        # self - s1 (5003), s2 (4006)
+        # self - current object address
+        return 'Name: ' + self.name + '\nGender: ' + self.gender + '\nRoll: ' + \
+            str(self.roll) + '\nMarks: ' + str(self.marks)
+
+    def get_grade(self):
+        marks = self.marks
+
+        if marks > 100 or marks < 0:
+            grade = 'I'
+        elif marks >= 70:
+            grade = 'A'
+        elif marks >= 60:
+            grade = 'B'
+        elif marks >= 40:
+            grade = 'C'
+        else:
+            grade = 'F'
+        return grade
---
+++ student_oop.py
@@ -0,0 +1,63 @@
+# Mehul
+from com.abc.college.student import Student
+
+# s1 = Student() # 5003 (RAM)
+# Internally
+# 1. RAM 5003 memory is reserved
+# 2. Student.__init__(5003)
+
+s1 = Student('mehul chopra', 10, 'm', 45)
+# Internally
+# 1. RAM 5003 memory is reserved
+# 2. Student.__init__(5003, 'mehul chopra', 10, 'm', 45)
+
+
+''' s1.name = 'mehul chopra'
+s1.roll = 10
+s1.gender = 'm'
+s1.marks = 45 '''
+
+
+s2 = Student('jane', 11, 'f', 67) # 4006 (RAM)
+''' s2.name = 'jane'
+s2.roll = 11
+s2.gender = 'f'
+s2.marks = 67 '''
+
+s3 = Student('roy', 15, 'm', 89) # 5005 (RAM)
+''' s3.student_name = 'roy'
+s3.r = 15
+s3.gen = 'm'
+s3.marks = 89 '''
+
+print(s3.get_details())
+# print(Student.get_details(s3))
+
+print(s1.get_details())
+
+# Internally
+# print(Student.get_details(s1))
+
+
+print(s2.get_details())
+
+print(s1.get_grade())
+# Internally
+# print(Student.get_grade(s1))
+
+print(s2.get_grade())
+# Internally
+# print(Student.get_grade(s2))
+
+# Internally
+# print(Student.get_details(s2))
+
+
+# print(s1)
+# print(s2)
+
+''' print(s2.name)
+print(s2.gender)
+
+print(s1.name)
+print(s1.gender) '''
